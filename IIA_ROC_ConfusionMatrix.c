01ea91fd2ea79f2d8ebba2588b1de1a8eb1567e9
luroel <jl.rodrigueze@hotmail.com>
2024-06-03 15:02:03 -0600

    Aplicando un clasificador vecino más cercano y evaluación del aprendizaje Curva ROC + Matriz de Confusión

--- IIA_ROC_ConfusionMatrix.c
+++ IIA_ROC_ConfusionMatrix.c
@@ -13,3 +13,12 @@ q=q(p);
 X = wq(1:10,:);
 Y = q(1:10);
 
+%Datos nuevos para el clasificiador
+mdl = ClassificationKNN.fit(X,Y,'NumNeighbors',5);
+Xn = wq(11:20,:);
+Yn = q(11:20);
+
+Yp = predict(mdl,Xn); %Encuentra a clase de mis nuevos datos (set entrena)
+
+[c,order] = confusionmat(Yn,Yp)
+rc = plotroc(Yn,Yp);
